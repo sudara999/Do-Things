1907f783bacaea5995e0d0fb7e4cccbb4185de7b
John Park <loopuntiltrue@gmail.com>
2020-05-25 02:45:52 +0900

    Added style for transition screens

--- DoThings_Application/TransitionToHelper.js
+++ DoThings_Application/TransitionToHelper.js
@@ -14,9 +14,12 @@ import {
 
 export default function TransitionToHelper({navigation}) {
     return (
-        <View>
-            <Text>Now we will show the perspective of the Helper</Text>
-            <Button title="Show Helper Screen" onPress={()=>navigation.navigate("CallScreen")}/>
+        <View style={{alignItems:"center",justifyContent:"space-between", flex:1}}>
+            <Text style={{padding:15, paddingTop: 40,fontSize:25, textAlign:"center"}}>For the next step, we will show the perspective of the person giving help</Text>
+            <Text style={{padding:15, fontSize:25, textAlign:"center"}}>First, the person will receive a notification that the person has asked for help.</Text>
+            <Text style={{padding:15, fontSize:25, textAlign:"center"}}>On Accepting, you will then be able to record your actions on the other person's phone.</Text>
+            <Button type="outline" title="Show Helper Screen" onPress={()=>navigation.navigate("CallScreen")}/>
+            <Button type="clear" title="Press this button to go back to the contacts page" onPress={()=>navigation.navigate("Contacts")}/>
         </View>
     );
 }
