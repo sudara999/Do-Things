a5488837d281f6170fb531571215837d904e35b6
John Park <loopuntiltrue@gmail.com>
2020-05-25 04:13:40 +0900

    Video playback in callscreen that moves to EndScreen on end (to be implemented)

--- DoThings_Application/screens/CallScreen.js
+++ DoThings_Application/screens/CallScreen.js
@@ -11,14 +11,35 @@ import {
   Alert,
   Share,
   Image,
-  ImageBackground
+  ImageBackground,
+  TouchableOpacity
 } from 'react-native';
+import VideoPlayer from 'react-native-video-controls';
 
-export default function CallScreen() {
-    return (
-        <View style={{flex:1, backgroundColor:"white", alignItems:"center"}}>
+export default class CallScreen extends React.Component {
+    constructor({ navigation }) {
+        super();
+        this.state = {
+          videoUri: null
+        }
+        this.navigation = navigation;
+      }
+    
+      handlePress = (path) => {
+        this.setState({ videoUri: path });
+        console.log(path);
+      }
+      playEnded = () => {
+        this.setState({ videoUri: null });
+        Alert.alert("TESTING ENDING");
+        // this.navigation.navigate("EndScreen");
+      }    
+    render() {
+        const videoUri = this.state.videoUri;
+        return(
+            <View style={{flex:1, backgroundColor:"white", alignItems:"center"}}>
             {/* <ImageBackground source={require("./img/callFrame.png")} style={{flex:1, alignItems:"center"}}> */}
-                <View style={{flex:1,top:100, backgroundColor:"white", width: 300, height: 300}}>
+                {!videoUri && <View style={{flex:1,top:100, backgroundColor:"white", width: 300, height: 300}}>
                     <View style={{backgroundColor: "white", flex: 1, justifyContent: "center", alignItems:"center"}}>
                         <Image source={require('../img/profileIcon.png')} style={{height:200, width:200}}/>
                     </View>
@@ -27,11 +48,22 @@ export default function CallScreen() {
                         <Image source={require('../img/askforhelp.png')} style={{top: 10}}/>
                     </View>
                     <View style={{flex: 1, flexDirection: "row", left:-30}}>
-                        <Image source={require('../img/accept.png')} style={{top: 10}}/>
+                        <TouchableOpacity onPress={this.handlePress}>
+                            <Image source={require('../img/accept.png')} style={{top: 10}}/>
+                        </TouchableOpacity>
                         <Image source={require('../img/decline.png')} style={{top: 10}}/>
                     </View>
-                </View>
+                </View>}
+                {videoUri && <VideoPlayer style={{}} source={{ uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" }} onEnd={this.playEnded}
+                            disableFullscreen={true}
+                            disablePlayPause={true}
+                            disableSeekbar={true}
+                            disableTimer={true}
+                            disableVolume={true}
+                            disableBack={true}
+                            />}     
             {/* </ImageBackground> */}
         </View>
-    );
+        );
+    };
 }
